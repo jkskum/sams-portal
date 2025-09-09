7dd712da52ca7df6e1741d105f776286eb3b0911
TuhsarChaudhary <tusharchaudhary161@gmail.com>
2025-09-09 14:36:31 +0530

    Initial commit of frontend with Login Component Screen

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+
+export const routes: Routes = [
+    {
+        path: 'auth/login',
+        component: LoginComponent
+    }
+];
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  showPassword = false;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+    if (this.email.valid && this.password.valid) {
+      console.log('Login form submitted: ' + this.email.value + ' ' + this.password.value);
+    }
+  }
+}
